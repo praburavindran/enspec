7aef914fb1dd9a1c44660c1142033168bb5e8ed9
praburavindran <praburavindran@gmail.com>
2015-02-20 14:56:55 -0600

    first test commit

---
+++ test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 17 11:03:40 2015
+
+@author: pravindran
+"""
+
+from __future__ import print_function
+
+
+def testfn():
+    print("Sri Ramajayam")
+        
+if __name__ == "__main__":
+    testfn()    
